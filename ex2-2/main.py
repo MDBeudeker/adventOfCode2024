165eaa2ef2facccb42bf150104aa2b5d3a8467f4
MDBeudeker <mauritsbeu@gmail.com>
2024-12-08 15:13:37 +0100

    refactor ex2-2, this still goes wrong

--- ex2-2/main.py
+++ ex2-2/main.py
@@ -9,66 +9,77 @@ with open(input, 'r') as fd:
   for row in reader:
     alist.append(row[0].split())
 
-total, check = 0,0
+total, check = 0, 0
+
+def safetyCheck(report, debug=0):
+  counter = 0
+  firstnumber, lastnumber = int(report[0]), int(report[-1])
+  
+  # determine whether report is ascending or descending
+  if firstnumber > lastnumber:
+    # descending
+    direction = -1
+  elif firstnumber < lastnumber:
+    # ascending
+    direction = 1
+  else:
+    # neither -> return False
+    return False
+  
+  # iterate through report
+  for data in report:
+    if counter > 0:
+        aold = a
+        a = int(data)
+        # if debug == 1:
+        #   print (aold, a, (direction * (a - aold)))
+        if (0 < (direction * (a - aold)) <= 3):
+          safe = True
+        else:
+          safe = False
+          break
+    elif counter == 0:
+      a = int(data)
+      counter += 1
+
+  if debug == 1:
+    print ("report {} is safe: {}".format(report, safe))
+  return safe
+
+def dampen(report):
+  print("result for report {} is false, trying to dampen".format(report))
+  for data in report:
+    # clone report so that original report is not modified
+    dampenedReport = report[:]
+    dampenedReport.remove(data)
+
+    # do a safety check on that particular dampened report
+    # print(dampenedReport)
+    safe = safetyCheck(dampenedReport, debug =1)
+    if safe == True:
+      print("received {} from report {} by dampening {}".format(safe, report, data))
+      return True
+  print("received {} from report {}".format(safe, report, data))
+  return False
+    
 
 
 for report in alist:
   # set safe to False
   safe = False
-  counter = 0
-  dampenerUsed = False
-  # check whether decreasing
-  if int(report[0]) > int(report[-1]):
-    for data in report:
-      if counter == 0:
-        a = int(data)
-        counter += 1
-      else:
-        aold = a
-        a = int(data)
-        print (aold, a) 
-        if (0 < (aold - a) <= 3):
-          safe = True
-        elif dampenerUsed == False:
-          dampenerUsed = True
-          print ("counter = {}".format(counter))
-          print ("dampener used on {}".format(a))
-          a = aold
-        else :
-          safe = False
-          break
-  # when increasing
-  if int(report[0]) < int(report[-1]):
-    for data in report:
-      if counter == 0:
-        a = int(data)
-        counter += 1
-      else: 
-        aold = a
-        a = int(data)
-        print (aold, a) 
-        if (0 < (a - aold) <= 3):
-          safe = True
-        elif dampenerUsed == False:
-          dampenerUsed = True
-          print ("counter = {}".format(counter))
-          print ("dampener used on {}".format(a))
-          a = aold
-        else :
-          safe = False
-          break
-  # edge case: start and end are the same = safe is False by default
-  if int(report[0]) == int(report[-1]):
-    check += 1
-    safe = False
-  print ("report {} is safe: {}".format(report, safe))
-
-
+  safe = safetyCheck(report)
   if safe == True:
     total += 1
+  else:
+    # if result is safe attempt to dampen
+    safe = dampen(report)
+    if safe == True:
+      total += 1
 
 print(total)
 print(check)
 
 ## 615 is too low
+## 624 is too low
+## 625 is too low
 ## 668 is too high
