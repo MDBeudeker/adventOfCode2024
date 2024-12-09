4d839cf11e7b87a5e43fba8171e7f67077a9dd1a
MDBeudeker <mauritsbeu@gmail.com>
2024-12-09 10:10:47 +0100

    still some errors

--- ex4-1/main.py
+++ ex4-1/main.py
@@ -1,13 +1,13 @@
 import csv
 
 # input = "input"
-input = "testinput3"
+input = "testinput"
 
 alist = []
 with open(input, 'r') as csv:
   content = csv.read()
 
-def findWord(content, word, width, height):
+def findWord(content, word, width, height, total):
   np = 0
   firstLetter = word[0]
 
@@ -16,38 +16,41 @@ def findWord(content, word, width, height):
       positionx = np % width
       positiony = round(np / width)
       # print("Found! {} at cell {} position x {} y {}".format(firstLetter, np, positionx, positiony))
-      searchLetters(content, word, np, positionx, positiony, width, height, width*height)
+      total = total + searchLetters(content, word, np, positionx, positiony, width, height, width*height)
     np += 1
+  return total
       
 
 
 def searchLetters(content, word, np, positionx, positiony, width, height, size):
   total = 0
-  counter = 1
   checkLeft = ((np - len(word) + 1) % width >= 0)
   checkRight = (np + len(word) + 1) % width < len(word)
   checkDown = ((np + (width * len(word)) + 1) < size)
   checkUp = ((np - (width * len(word[1:])) + 1) > 0)
+  #left
+  counter = 1
   for char in word[1:]:
-    #left
     if (checkLeft):
       nextValue = np-counter
       if char == content[nextValue]:
         counter += 1
         if (counter == len(word)) & (char == word[-1]):
           print("Found! {} at position {} x {} y {} going left".format(word, np, positionx, positiony))
+          total +=1
+  # right
   counter = 1
   for char in word[1:]:
-    # right
     if (checkRight):
       nextValue = np+counter
       if char == content[nextValue]:
         counter += 1
         if (counter == len(word)) & (char == 'S'):
           print("Found! {} at position {} x {} y {} going right".format(word, np, positionx, positiony))
+          total +=1
+  # down
   counter = 1
   for char in word[1:]:
-  # down
     if (checkDown):
       nextValue = np+(width * counter)
       # print("found X at {}; going down, next value is {}".format(np, content[np+(width * counter)]))
@@ -56,10 +59,11 @@ def searchLetters(content, word, np, positionx, positiony, width, height, size):
         counter += 1
         if (counter == len(word)) & (char == 'S'):
           print("Found! {} at position {} x {} y {} going down".format(word, np, positionx, positiony))
+          total +=1
   # up
   counter = 1
   for char in word[1:]:
-    print("up {}".format((np - (width * len(word)) + 1)))
+    # print("up {}".format((np - (width * len(word)) + 1)))
     if checkUp:
       nextValue= np-(width * counter)
       # print("found X at {}; going up, next value is {}".format(np, content[np-(width * counter)]))
@@ -68,16 +72,20 @@ def searchLetters(content, word, np, positionx, positiony, width, height, size):
         counter += 1
         if (counter == len(word)) & (char == 'S'):
           print("Found! {} at position {} x {} y {} going up".format(word, np, positionx, positiony))
+          total +=1
+  # down-right
   counter = 1
   for char in word[1:]:
-    # down-right
     if checkDown & checkRight:
       nextValue = (np+(width * counter)+ counter)
+      # deze checken, index out of range
+      print("going down-right at x {} y {}, next value is {}".format(positionx, positiony, content[nextValue]))
       if char == content[nextValue]:
         print (char, nextValue)
         counter += 1
         if (counter == len(word)) & (char == 'S'):
           print("Found! {} at position {} x {} y {} going down-right".format(word, np, positionx, positiony))
+          total +=1
   # up-left
   counter = 1
   for char in word[1:]:
@@ -89,33 +97,49 @@ def searchLetters(content, word, np, positionx, positiony, width, height, size):
         counter += 1
         if (counter == len(word)) & (char == 'S'):
           print("Found! {} at position {} x {} y {} going up-left".format(word, np, positionx, positiony))
+          total +=1
+  # down-left
+  counter = 1
   for char in word[1:]:
-    # down-left
     if checkDown & checkLeft:
       nextValue = np+(width * counter)-counter
+      # if nextValue < size:
+      #   break
+      print("xposition: {} yposition: {} nextValue: {} size: {}".format(positionx, positiony, nextValue, size))
+      print((np - len(word) + 1) % width)
+      print((np + (width * len(word)) + 1))
+      print(nextValue)
+      # deze checken, index out of range
       if char == content[nextValue]:
         print (char, nextValue)
         counter += 1
         if (counter == len(word)) & (char == 'S'):
           print("Found! {} at position {} x {} y {} going down-left".format(word, np, positionx, positiony))
+          total +=1
   # up-right
   counter = 1
   for char in word[1:]:
     if checkUp & checkRight:
       nextValue = np-(width * counter)+ counter
-      # print("found X at {}; going up-left, next value is {}".format(np, content[np-(width * counter)-counter]))
+      # print("found X at {}; going up-right, next value is {}".format(np, content[np-(width * counter)-counter]))
       if char == content[nextValue]:
         # print (char, np+(width * counter))
         counter += 1
         if (counter == len(word)) & (char == 'S'):
-          print("Found! {} at position {} x {} y {} going up-left".format(word, np, positionx, positiony))
+          print("Found! {} at position {} x {} y {} going up-right".format(word, np, positionx, positiony))
+          total +=1
+  return total
 
     
 
-# print(repr(content))
+# replace line endings to work independent of unix and windows
+content = content.replace("\r\n", "\n").replace("\r", "\n")
+
 width = len(content.split("\n")[0])
 height = len(content.split("\n"))
 print(width, height)
 content = content.replace("\n", "")
 
-findWord(content, "XMAS", width, height)
+total = 0
+total = findWord(content, "XMAS", width, height, total)
+print("Total: {}".format(total))
